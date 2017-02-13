192542b5869353e8c3ed01ead95fd05ed5aaddad
Nicholas Soffa <nsoffa@lion.lmu.edu>
2017-02-12 23:35:48 -0800

    Knockback sort of working. Knocks up but not away

--- Project Files/Space Runner/Assets/Scripts/HealthSystem.cs
+++ Project Files/Space Runner/Assets/Scripts/HealthSystem.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class HealthSystem : MonoBehaviour {
@@ -7,11 +6,14 @@ public class HealthSystem : MonoBehaviour {
 	private int maxHealth = 5;
 	public int currentHealth;
 
+	private Rigidbody2D rb2d;
+
 	public LayerMask enemyLayer;
 	public LayerMask obstacleLayer;
 
 	void Start () {
 		currentHealth = maxHealth;
+		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -29,5 +31,16 @@ public class HealthSystem : MonoBehaviour {
 
 	void Die() {
 		Destroy (gameObject);
+	}
+
+	public IEnumerator Knockback(float knockDuration, float knockPower, Vector3 knockDirection) {
+		float time = 0;
+
+		while (knockDuration > time) {
+			time += Time.deltaTime;
+			rb2d.AddForce (new Vector3 (knockDirection.x * -100, knockDirection.y + knockPower, transform.position.z));
+		}
+
+		yield return 0;
 	}
 }
--- Project Files/Space Runner/Assets/Scripts/Obstacle.cs
+++ Project Files/Space Runner/Assets/Scripts/Obstacle.cs
@@ -12,6 +12,8 @@ public class Obstacle : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D enter) {
 		if (enter.CompareTag ("Player")) {
 			hero.Damage (1);
+
+			StartCoroutine (hero.Knockback (0.02f, 350f, hero.transform.position));
 		}
 	}
 }
