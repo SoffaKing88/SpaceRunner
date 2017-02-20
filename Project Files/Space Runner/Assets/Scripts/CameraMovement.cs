9f9a713ba40f528097879fbc00eb7698906150e6
Nicholas Soffa <nsoffa@lion.lmu.edu>
2017-02-20 15:24:21 -0800

    Introducing Camera Movement

---
+++ Project Files/Space Runner/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private GameObject player;
+	private float moveSpeed = 0.8f;
+
+	private Vector3 gameDirection = Vector3.right;
+
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+
+	void Update () {
+		transform.Translate (gameDirection * moveSpeed * Time.deltaTime);
+	}
+
+}
