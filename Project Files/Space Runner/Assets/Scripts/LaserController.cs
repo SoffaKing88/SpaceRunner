36bd80ef402b378f793641ee8689fcd7ae172d8b
Nick Soffa <nsoffa@lion.lmu.edu>
2017-03-27 18:41:51 -0700

    Laser is working

---
+++ Project Files/Space Runner/Assets/Scripts/LaserController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserController : MonoBehaviour {
+
+	public float laserDelay;
+	public float laserFireTime;
+
+	private Collider2D laser;
+
+	void Start() {
+		laser = GetComponent<Collider2D> ();
+		laser.enabled = false;
+		StartCoroutine (FireLaser ());
+	}
+
+	IEnumerator FireLaser() {
+		laser.enabled = !laser.enabled;
+		yield return new WaitForSeconds (laserFireTime);
+		laser.enabled = !laser.enabled;
+		yield return new WaitForSeconds (laserDelay);
+		StartCoroutine (FireLaser ());
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.isTrigger != true) {
+			if (col.CompareTag ("Player")) {
+				col.GetComponent<HealthSystem> ().Damage (1);
+			}
+		}
+	}
+}
