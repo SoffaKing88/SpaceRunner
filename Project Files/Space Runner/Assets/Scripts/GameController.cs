e9940404fa83bdcafa45207c90a45d0df00c3e49
Nicholas Soffa <nsoffa@lion.lmu.edu>
2017-04-17 16:08:43 -0700

    Some initial room spawning works, will work on better algorithm soon

--- Project Files/Space Runner/Assets/Scripts/GameController.cs
+++ Project Files/Space Runner/Assets/Scripts/GameController.cs
@@ -5,8 +5,21 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
 	public int gemAmount = 0;
-
 	public Text gemText;
+
+	public GameObject[] levels;
+	private Transform spawnPoint;
+	private int index;
+
+	void Start(){
+		spawnPoint = GetComponent<Transform> ();
+		
+		for (int i = 0; i < 5; i++) {
+			index = Random.Range (0, levels.Length);
+			spawnPoint.position = new Vector3 (37f * i, 0, 0);
+			Instantiate (levels [index], spawnPoint.position, spawnPoint.rotation);
+		}
+	}
 
 	void Update() {
 		//gemText.text = ("Gems: " + gemAmount);
