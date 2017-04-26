0869c93c86c2ec7cc040dba85148b16c6bad8bfe
Nick Soffa <nsoffa@lion.lmu.edu>
2017-04-26 15:56:32 -0700

    Swapping over to desktop to work on Enemy Spawning

---
+++ Project Files/Space Runner/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+
+	private GameController gControl;
+	private float chance;
+	private float spawn = 70f;
+	private GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+		gControl = GameObject.Find ("GameController").GetComponent<GameController>();
+		enemies = gControl.enemies;
+		chance = Random.Range (0f, 100f);
+		for (int i = 0; i < spawnPoints.Length; i++) {
+			if (chance > spawn) {
+				Instantiate (enemies [Random.Range (0, enemies.Length)], spawnPoints [i].position, spawnPoints[i].rotation);
+				spawn += 15;
+			} else {
+				spawn -= 20;
+			}
+		}
+	}
+}
