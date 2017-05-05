d7eebd24845695881279bb98a133efdce8928df7
Nicholas Soffa <nsoffa@lion.lmu.edu>
2017-05-05 10:33:50 -0700

    Health UI working

---
+++ Project Files/Space Runner/Assets/Scripts/Game/HUD.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+	public Sprite[] heartSprites;
+
+	public Image heartUI;
+
+	private HealthSystem hero;
+
+	void Start() {
+		hero = GameObject.FindGameObjectWithTag ("Player").GetComponent<HealthSystem> ();
+	}
+
+	void Update() {
+		heartUI.sprite = heartSprites [hero.currentHealth];
+	}
+}
