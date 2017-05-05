b52739b94b1e77a518c74de9ca542a65c5d1a86d
Nicholas Soffa <nsoffa@lion.lmu.edu>
2017-05-04 23:12:20 -0700

    Explosion sound effect working

--- Project Files/Space Runner/Assets/Scripts/Game/Explosion.cs
+++ Project Files/Space Runner/Assets/Scripts/Game/Explosion.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class Explosion : MonoBehaviour {
 
-	private Animation anim;
+	private SpriteRenderer sprite;
 
 	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
 		StartCoroutine (Wait ());
 	}
 
 	IEnumerator Wait(){
 		yield return new WaitForSeconds (0.2f);
+		sprite.enabled = false;
+		yield return new WaitForSeconds (2f);
 		Destroy (gameObject);
 	}
 }
