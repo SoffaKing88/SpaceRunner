413d710474892d88ee8833d6bff46d610879e9b2
Nicholas Soffa <nsoffa@lion.lmu.edu>
2017-05-05 11:39:24 -0700

    Scrolling background

---
+++ Project Files/Space Runner/Assets/Scripts/Game/BgScroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgScroller : MonoBehaviour {
+
+	private GameController gc;
+	public GameObject nextBackground;
+	private bool sent;
+
+	// Use this for initialization
+	void Start () {
+		gc = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate ((Vector3.left * gc.gameSpeed * Time.deltaTime) / 2, Space.World);
+		if (transform.position.x < -13 && !sent) {
+			Instantiate (nextBackground, new Vector3 (transform.position.x + 61, transform.position.y, 30), transform.rotation);
+			sent = true;
+		} else if (transform.position.x < -48) {
+			Destroy (gameObject);
+		}
+	}
+}
